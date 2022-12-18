427385a8a3eb5d503014a0942437472a62dc3ebd
Mansur0777 <prok47432@gmail.com>
2022-12-18 21:34:50 +0500

    neam

--- app/src/main/java/com/example/biglafe/MainActivity2.java
+++ app/src/main/java/com/example/biglafe/MainActivity2.java
@@ -16,6 +16,7 @@ import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -23,12 +24,14 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class MainActivity2 extends AppCompatActivity {
 BottomNavigationView bottomNavigationView;
 FloatingActionButton fab;
+ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         bottomNavigationView=findViewById(R.id.bottomNavigationView);
         fab=findViewById(R.id.fab);
+        imageView=findViewById(R.id.image);
 
         if (ContextCompat.checkSelfPermission(MainActivity2.this,
                 Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
@@ -55,7 +58,7 @@ FloatingActionButton fab;
 
         if (requestCode == 100) {
             Bitmap captureImage = (Bitmap) data.getExtras().get("data");
-            fab.setImageBitmap(captureImage);
+            imageView.setImageBitmap(captureImage);
         }
 
     }
